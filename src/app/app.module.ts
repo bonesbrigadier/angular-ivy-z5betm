8e95626cb7a3d3bc23535c0a3c029e21165375b9
bonesbrigadier <g@silversquid.com>
2022-08-24 12:27:30 +0100

    Added ReactiveForms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LikeComponent } from './like/like.component';
 import { BookmarkComponent } from './bookmark/bookmark.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   declarations: [AppComponent, LikeComponent, BookmarkComponent],
   bootstrap: [AppComponent],
 })
