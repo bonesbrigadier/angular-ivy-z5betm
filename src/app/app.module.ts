b0e0d902c41e6e1be0a2c6ee7736994c0879ed90
bonesbrigadier <g@silversquid.com>
2022-08-25 07:05:30 +0100

    wired search results component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LikeComponent } from './like/like.component';
 import { BookmarkComponent } from './bookmark/bookmark.component';
 import { SearchComponent } from './search/search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   imports: [BrowserModule, ReactiveFormsModule],
@@ -14,6 +15,7 @@ import { SearchComponent } from './search/search/search.component';
     LikeComponent,
     BookmarkComponent,
     SearchComponent,
+    SearchResultsComponent,
   ],
   bootstrap: [AppComponent],
 })
