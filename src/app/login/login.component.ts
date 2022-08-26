5bd5767f721565e1b63d5c37cf76c69bf7f93dbc
bonesbrigadier <g@silversquid.com>
2022-08-26 07:56:58 +0100

    Login implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loggedInUserSubject = new BehaviorSubject<boolean>(false);
+  loggedInUser$ = of(null);
 
+  login() {
+    this.loggedInUserSubject.next(true);
+  }
+
+  logout() {
+    this.loggedInUserSubject.next(false);
+  }
 }
